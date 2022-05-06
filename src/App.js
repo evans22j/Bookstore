01376958eddea4ef3b856cf9b15143046b0b014c
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-06 23:46:23 +0200

    add information to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Categories from './components/Categories/Categories';
 import Books from './components/Book/Books';
@@ -6,13 +6,13 @@ import Heading from './components/heading/Heading';
 
 const App = () => (
   <div>
-  <Router>
-    <Heading />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/Categories" element={<Categories />} />
-    </Routes>
-  </Router>
+    <Router>
+      <Heading />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Router>
   </div>
 );
 
