164dfb7046b435be84d47e4ee8376ee8079861e6
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-10 22:44:57 +0200

    fix linter errors

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { generate } from 'randomized-string';
 import { alphanumeric } from 'randomized-string/lib/types';
